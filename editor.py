c6b4e83cfca8c1331568b199b768365dbba9452d
Avery Seymour <poofymeowmeow@gmail.com>
2025-10-30 15:18:20 +0000

    initial commit, add basic functionality

---
+++ editor.py
@@ -0,0 +1,82 @@
+import curses
+import sys
+import argparse
+
+class Window:
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+
+class Cursor:
+    def __init__(self, row=0, col=0, col_hint=None):
+        self.row = row
+        self.col = col
+        self.col_hint = col if col_hint is None else col_hint
+    
+    @property
+    def col(self):
+        return self._col
+    
+    @col.setter
+    def col(self, col):
+        self._col = col
+        self._col_hint = col
+
+    def up(self, buffer):
+        if self.row > 0:
+            self.row -= 1
+            self._clamp_col(buffer)
+    
+    def down(self, buffer):
+        if self.row < len(buffer) - 1:
+            self.row += 1
+            self._clamp_col(buffer)
+    
+    def _clamp_col(self, buffer):
+        self.col = min(self._col_hint, len(buffer[self.row]))
+    
+    def left(self, buffer):
+        if self.col > 0:
+            self.col -= 1
+        elif self.row > 0:
+            self.row -= 1
+            self.col = len(buffer[self.row])
+
+    def right(self, buffer):
+        if self.col < len(buffer[self.row]):
+            self.col += 1
+        elif self.row < len(buffer) - 1:
+            self.row += 1
+            self.col = 0
+
+def main(stdscr):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        buffer = f.readlines()
+
+    window = Window(curses.LINES - 1, curses.COLS - 1)
+    cursor = Cursor()
+
+    while True:
+        stdscr.erase()
+        for row, line in enumerate(buffer[:window.n_rows:]):
+            stdscr.addstr(row, 0, line[:window.n_cols:])
+        stdscr.move(cursor.row, cursor.col)
+
+        k = stdscr.getkey()
+        if k == "q":
+            sys.exit(0)
+        elif k == "KEY_UP":
+            cursor.up(buffer)
+        elif k == "KEY_DOWN":
+            cursor.down(buffer)
+        elif k == "KEY_LEFT":
+            cursor.left(buffer)
+        elif k == "KEY_RIGHT":
+            cursor.right(buffer)
+
+if __name__ == "__main__":
+    curses.wrapper(main)
